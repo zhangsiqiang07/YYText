dc03783d707bc1840dd9d37df75d1418157ffecc
啊强小衰flynn <1328254616@qq.com>
2026-03-30 16:30:27 +0800

    fix:YYTextLayout <<<
    修复xcode26.4报错

--- YYText/Component/YYTextLayout.m
+++ YYText/Component/YYTextLayout.m
@@ -1505,17 +1505,17 @@ fail:
     
     [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            position = (fabs(left - point.y) < fabs(right - point.y)) < (right ? prev : next);
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            position = (fabs(left - point.x) < fabs(right - point.x)) < (right ? prev : next);
         }
     }];
     
     [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            position = (fabs(left - point.y) < fabs(right - point.y)) < (right ? prev : next);
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            position = (fabs(left - point.x) < fabs(right - point.x)) < (right ? prev : next);
         }
     }];
     
